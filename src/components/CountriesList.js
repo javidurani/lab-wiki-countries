2cd637aac7da88053bf307e18c945f7261735fb9
javidurani <jduraniglesias21122@columbushs.com>
2021-03-03 14:24:24 -0800

    commit

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -2,19 +2,19 @@ import countries from '../countries.json'
 import React from 'react';
 
 function CountriesList(){
-    const [countries, setCountries] = React.useState();
+    const [country, setCountry] = React.useState();
 
     return(
         <div className = 'list'>
-            {countries.map(c => {
+            {country.map(c => {
                 return(
                     <div className = "child">
                         <ul>{c.flag}{c.common}</ul>
                     </div>
                 )
-            })}
+            })}; 
         </div>
-    )
+    );
 }
 
 export default CountriesList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import logo from './logo.svg';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+import Navbar from './components/Navbar.js'
+import './components/Navbar.css'
+import CountriesList from './components/CountriesList'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
